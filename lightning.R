e812c7407a5efaf66b0772bb9d29a3a5a262bd96
Mike Loper <michael.loper@uwrf.edu>
2025-12-11 10:32:27 -0600

    Updated cleaner version
    removed unnecessary code, added comments, added code to match paper

--- lightning.R
+++ lightning.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(matlib)
 
 # this is a function that calculates the probability that each player
-# is eliminated in a game with n players
+# is eliminated in a game with n players, n >= 3
 lightningelim <- function(SS_make, LS_make, n) {
   p <- LS_make
   q <- SS_make
@@ -47,7 +47,7 @@ lightningelim <- function(SS_make, LS_make, n) {
   return(x[1,])
 }
 
-# Calculates each player's probability of winning
+# Calculates each player's probability of winning, n >= 2
 lightning <- function(SS_make, LS_make, n) {
  W <- matrix(c(1.0/(3.0-LS_make), 1-1.0/(3.0-LS_make)), nrow = 1, ncol = 2)
  count <- 2
@@ -71,11 +71,12 @@ lightning <- function(SS_make, LS_make, n) {
 lightningtimetoelim <- function(SS_make, LS_make, n) {
   p <- LS_make
   q <- SS_make
-  #need to make function for n=2
+  #Case of n=2
   if (n == 2) {
     rounds <- (-1)*(p+q-3)*(p^2-p*q-2*p+2*q+1) / (q*(p-3)*(p-1)*(p-q+1))
     return(rounds)
   }
+  #Case of n greater than 2
   else {
     Tr <- diag(6*n)
     for (i in 1:(5*n)) Tr[i,i] = 0
@@ -117,6 +118,7 @@ lightningtimetoelim <- function(SS_make, LS_make, n) {
     return(x[1,])
   }
 }
+
 # number of rounds for longest game
 lightningtimetoelim(0.9,0.4,2)*700
 
@@ -167,14 +169,68 @@ data10players9080 <- data.frame(
   Position = c(1:10),
   Probability = c(lightning(0.9,0.8,10))
 )
-ggplot(data10players9020,aes(Position,Probability)) +
-  geom_point(size=2, shape = 16) + 
-  geom_line() +
-  geom_point(data=data10players9050,size=2,color='black',shape = 15) +
-  geom_line(data=data10players9050,color='black') +
-  geom_point(data=data10players9080,size=3,color='black',shape=17) +
-  geom_line(data=data10players9080,color='black')+
+
+combined_df <- bind_rows(
+  data10players9020 %>% mutate(Legend = "p=0.20"),
+  data10players9050 %>% mutate(Legend = "p=0.50"),
+  data10players9080 %>% mutate(Legend = "p=0.80")
+)
+
+ggplot(combined_df,aes(x=Position,y=Probability, shape = Legend)) +
+  geom_point(size=3) + 
+  geom_line()+
   scale_x_continuous(breaks = seq(0,10))+
   theme_bw()+
-  labs(title="10 Player Game",
-       shape="Long Shot")
+  labs(title="Probability of Winning a 10 Player Game by Starting Position and Long Shot Percentage",
+       shape="Long Shot")+
+  xlab("Starting Position")
+
+## checking if first really is the worst
+## First is the worst for 2 players (see Thm 3.1)
+count <- 0
+total <- 0
+for (n in seq(3,60)) {
+  print(n)
+  for (i in seq(0.01,1.00,0.01)) {
+    for (j in seq(0.00,0.99,0.01)) {
+      if (i != 1.00 | j != 0.00) {
+        total <- total + 1
+        if (which.min(lightning(i,j,n)) != 1) {
+          count <- count + 1
+          print(c(i,j,n))
+        }
+      }
+    }
+  }
+}
+## first is the worst for n<= 60 players and prob step size 0.01
+
+## same loop is used to check how often last is the best
+# from 3 to 25, 10139 out of 229977 do not have last as best
+# from 3 to 30, 10385 out of 279972 do not have last as best
+# from 3 to 35, 10674 out of 329967 do not have last as best
+# from 3 to 45, 11027 out of 429957 do not have last as best
+# from 3 to 50, 11138 out of 479952 do not have last as best
+# from 3 to 60, 11367 out of 579942 do not have last as best
+# in all cases where last is not best, second to last was best
+#    and this only occurred when n was odd, q is large and p is small
+count <- 0
+total <- 0
+for (n in seq(3,60)) {
+  print(n)
+  for (i in seq(0.01,1.00,0.01)) {
+    for (j in seq(0.00,0.99,0.01)) {
+      if (i != 1.00 | j != 0.00) {
+        total <- total + 1
+        if (which.max(lightning(i,j,n)) != n) {
+          count <- count + 1
+          print(c(i,j,n))
+        }
+      }
+    }
+  }
+}
+print(total)
+print(count)
+print(count/total)
+
